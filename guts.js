19dc83cf9abb40244893dd351b36a6dee2d2d0b5
Meka Seba <mekasebameka@gmail.com>
2022-07-15 17:57:20 -0700

    a start..

--- guts.js
+++ guts.js
@@ -3,3 +3,16 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
+
+/*let optionTwo = document.getElementById("option-two")
+
+password.addEventListener("click", function() {
+    optionOne.textContent = ""
+    optionTwo.textContent = ""
+    for (let i = 0; i < 15; i++) {
+    const randomOne = Math.floor(Math.random() * characters.length)
+    const randomTwo = Math.floor(Math.random() * characters.length)
+    optionOne.textContent += characters[randomOne]
+    optionTwo.textContent += characters[randomTwo]    
+   
+}})
